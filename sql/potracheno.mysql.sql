15f831481a80d5fdd0cc46f2832f247b1be0821e
Konstantin S. Uvarin <khedin@gmail.com>
2016-09-26 13:51:25 +0300

    MOD Add mysql DB schema

---
+++ sql/potracheno.mysql.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20) UNIQUE,
+    password varchar(256),
+    created INT -- unix time
+) DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS issue;
+CREATE TABLE issue (
+    issue_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    summary varchar (256) NOT NULL,
+    body varchar(4096) NOT NULL,
+    user_id INT,
+    status_id INT default 1,
+    created INT -- unix time
+) DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS activity;
+CREATE TABLE activity (
+    activity_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    issue_id INT NOT NULL,
+    seconds INT,
+    fix_estimate INT,
+    note varchar(4096),
+    created INT -- unix time
+) DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS sess;
+CREATE TABLE sess (
+    sess_id varchar(22) PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    created INT -- unix time
+) DEFAULT CHARSET=utf8;
